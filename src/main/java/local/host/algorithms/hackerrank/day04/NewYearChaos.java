a75be72e0f61c16f94d71c76beb98ee79126bbc7
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-01-17 02:32:48 -0300

    Tentando aplicar a melhor forma para resolver o desafio do hackerhank. Um caso ainda falha. O último algoritmo aprendido no livro não pode ser aplicado para este caso, pois este é um caso de ordenação e não de encontrar o menor caminho.

--- src/main/java/local/host/algorithms/hackerrank/day04/NewYearChaos.java
+++ src/main/java/local/host/algorithms/hackerrank/day04/NewYearChaos.java
@@ -7,23 +7,26 @@ public class NewYearChaos {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(List.of(2, 1, 5, 3, 4));
         // 2, 1, 5, 3, 4
-
         // 1, 2, 3, 4, 5
-
         // 0, 1, 2, 3, 4
         List<Integer> arr02 = new ArrayList<>(List.of(2, 5, 1, 3, 4));
 
         List<Integer> arr03 = new ArrayList<>(List.of(1, 2, 5, 3, 7, 8, 6, 4));
 
-        // 1, 2, 5, 3, 7, 8, 6, 4
         // 1, 2, 3, 4, 5, 6, 7, 8
-        // 0  0  2 -1  2  2 -1 -4
-
         // 1, 2, 5, 3, 4, 6, 7, 8 -> 2
         // 1, 2, 5, 3, 6, 4, 7, 8 -> 1
         // 1, 2, 5, 3, 7, 6, 4, 8 -> 2
         // 1, 2, 5, 3, 7, 8, 6, 4 -> 2
 
+
+        // 1, 2, 5, 3, 7, 8, 6, 4
+
+
+        // 1, 2, 5, 3, 7, 6, 4, 8 -> 2   * O 8 muda 2 posições
+        // 1, 2, 5, 3, 6, 4, 7, 8 -> 2   * O 7 muda 2 posições
+        // 1, 2, 5, 3, 4, 6, 7, 8 -> 1
+        // 1, 2, 3, 4, 5, 6, 7, 8 -> 2
         minimumBribes(arr03);
     }
 
@@ -31,18 +34,16 @@ public class NewYearChaos {
         int bribes = 0;
         int size = q.size();
 
-        for (int i = 0; i < size; i++) {
-
-            if (i + 1 < q.get(i) - 2) {
-                System.out.println("Too chaotic");
-                return;
-            }
-
-            else if (i + 1 < q.get(i)) {
-                bribes += q.get(i) - i - 1;
+        for (int i = size; i > 0; i--) {
+            if(q.get(i - 1) > i){
+                int bribe = q.get(i - 1) - i;
+                if(bribe > 2){
+                    System.out.println("Too chaotic");
+                    return;
+                }
+                bribes += bribe;
             }
         }
         System.out.println(bribes);
-
     }
 }
